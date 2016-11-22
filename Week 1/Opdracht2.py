06005f858f2c0cf9c188a4167e14ba61fd38e2a0
JamieKalloe <Jamie@Kalloe.net>
2016-11-22 11:40:57 +0100

    finished exercise 2 of week 1

--- Week 1/Opdracht2.py
+++ Week 1/Opdracht2.py
@@ -1,8 +1,19 @@
+import math
+
 def main():
-    radialen = int(input("Voer het aantal radialen in: "))
-    print(radialenNaarGraden(radialen))
+    radialen = float(input("Voer het aantal radialen in: "))
+    graden = radialenNaarGraden(radialen)
+    minuten = gradenNaarTijd(graden)
+    seconden = gradenNaarTijd(minuten)
+
+    print(graden, minuten, seconden)
 
 def radialenNaarGraden(radialen):
-    return 0
+    graden = (180 / math.pi) * radialen
+    return graden
+
+def gradenNaarTijd(graden):
+    minuten = (round(graden, 3) - int(graden)) * 60
+    return minuten
 
 main()
